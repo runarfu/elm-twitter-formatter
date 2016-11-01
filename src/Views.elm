ad06df014e99e0863f2e5acbde9396cca80fd4da
ruf <ruf@knowit.no>
2016-11-01 15:05:10 +0100

    Highlighting when > 140 characters

--- src/Views.elm
+++ src/Views.elm
@@ -8,6 +8,10 @@ import Char
 import Types exposing (..)
 
 
+tweetMaxLength =
+    140
+
+
 view : Model -> Html Msg
 view model =
     div []
@@ -20,7 +24,7 @@ view model =
 viewFormatted : Model -> Html Msg
 viewFormatted model =
     div []
-        (List.map (\f -> p [] [ text (f model) ]) functions)
+        (List.map (\f -> p [] [ addColorToLongStrings (f model) ]) functions)
 
 
 functions : List (String -> String)
@@ -29,3 +33,21 @@ functions =
     , String.map Char.toUpper
     , String.words >> List.intersperse "👏" >> String.join " "
     ]
+
+
+addColorToLongStrings : String -> Html Msg
+addColorToLongStrings s =
+    if String.length s > tweetMaxLength then
+        let
+            okPart =
+                String.dropRight tweetMaxLength s
+
+            notOkPart =
+                String.dropLeft tweetMaxLength s
+        in
+            p []
+                [ text okPart
+                , span [ style [ ( "background-color", "red" ) ] ] [ text notOkPart ]
+                ]
+    else
+        p [] [ text s ]

777c157d1da18115546c61df4c2b2f92f0c1da33
ruf <ruf@knowit.no>
2016-11-01 14:45:21 +0100

    Simple formatting functions

--- src/Views.elm
+++ src/Views.elm
@@ -3,9 +3,29 @@ module Views exposing (view)
 import Html exposing (..)
 import Html.Events exposing (..)
 import Html.Attributes exposing (..)
+import String
+import Char
 import Types exposing (..)
 
 
 view : Model -> Html Msg
 view model =
-    h1 [] [ text "Tweet Formatter" ]
+    div []
+        [ h1 [] [ text "Twitter Formatter" ]
+        , p [] [ textarea [ onInput SetText ] [ text model ] ]
+        , viewFormatted model
+        ]
+
+
+viewFormatted : Model -> Html Msg
+viewFormatted model =
+    div []
+        (List.map (\f -> p [] [ textarea [] [ text (f model) ] ]) functions)
+
+
+functions : List (String -> String)
+functions =
+    [ String.reverse
+    , String.map Char.toUpper
+    , String.words >> List.intersperse "👏" >> String.join " "
+    ]
